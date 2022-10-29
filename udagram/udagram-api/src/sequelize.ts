6c93a69031c344193f2ac144275233c130af19e3
Fatma Yasser <fatemayasser8976@gmail.com>
2022-10-29 17:42:50 +0200

    udagram

---
+++ udagram/udagram-api/src/sequelize.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
+
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  port: config.db_port as unknown as number,
+
+  dialect: "postgres",
+  storage: ":memory:",
+});
